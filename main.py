6ed612555292368e970feb835916b41e29fd9d4b
Steven Ouyang <stevenouy@hotmail.com>
2021-01-04 10:23:24 -0800

    Committing the base of the game

---
+++ main.py
@@ -0,0 +1,23 @@
+# main.py
+# midnight rider
+# A text adventure game that is riveting
+# IGN gives it 4 stars out of 100.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    pass
+
+
+    # Show introduction
+
+    # LOOP
+    # Check if reached END GAME
+
+    # Give the player their choices
+
+    #Change the environment based on choice and RNG
+if __name__ == '__main__':
+    main('PyCharm')
